fc99c4612d80cb4f022bf4945e10d88113c285e5
Israel <213withani@gmail.com>
2017-08-22 22:40:10 -0700

    Added update functionality with textarea and button

--- public/lectures/firstAngular/app.js
+++ public/lectures/firstAngular/app.js
@@ -4,25 +4,40 @@
         .controller("TodoListController",todoListController);
 
     function todoListController($scope) {
-        $scope.inputTodo = {title:"initial title"};
+        $scope.inputTodo = {title:"initial title",details:"initial details"};
         $scope.todos = [];
         // mapping view to local fn
         $scope.addTodo = addTodo;
         $scope.removeTodo = removeTodo;
+        $scope.selectTodo=selectTodo;
+        $scope.updateTodo=updateTodo;
 
         function addTodo(inputTodo) {
 
-            var newTodo = {
-                title:inputTodo.title
-            }
+            // var newTodo = {
+            //     title:inputTodo.title
+            // }
 
+            var newTodo = angular.copy(inputTodo)
             console.log(newTodo);
 
             $scope.todos.push(newTodo);
         }
 
+        // todo is the loop index from the view
         function removeTodo(todo) {
-            $scope.todos.splice(todo,1);
+            //calcuate index since todo is an obj and splice needs an index
+            var index=$scope.todos.indexOf(todo)
+            $scope.todos.splice(index,1);
+        }
+
+        function selectTodo(index) {
+            $scope.inputTodo=angular.copy($scope.todos[index]);
+            $scope.selectedIndex=index;
+        }
+
+        function updateTodo(todo) {
+            $scope.todos[$scope.selectedIndex]=angular.copy(todo);
         }
     }
 })();
